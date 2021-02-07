8d79e4ab6d32a98be16feca77dc897cd3ce90ab7
EntireTwix <willkatz.dev@gmail.com>
2021-02-07 11:15:11 -0800

    :art: removed imgui backend comments

--- main.cpp
+++ main.cpp
@@ -56,8 +56,6 @@ bool Load(const std::string &location, T &dest)
     return true;
 }
 
-//static bool show_demo_window = true;
-
 static const ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 static constexpr ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoTitleBar;
 static constexpr ImGuiWindowFlags default_flags = ImGuiWindowFlags_NoCollapse;
@@ -208,9 +206,6 @@ void my_display_code()
             task_buffer.push_back({t});
             change_flag = true;
         }
-        // static float r, g;
-        // ImGui::SliderFloat("red", &r, 0, 5);
-        // ImGui::SliderFloat("green", &g, 0, 5);
 
         if (ImGui::BeginTable("##table1", 1 + values.size(), table_settings))
         {
@@ -324,9 +319,6 @@ void my_display_code()
         }
         ImGui::End();
     }
-
-    // if (show_demo_window)
-    //     ImGui::ShowDemoWindow(&show_demo_window);
 }
 
 void glut_display_func()
@@ -351,18 +343,8 @@ void glut_display_func()
     glutPostRedisplay();
 }
 
-// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-
 int main(int argc, char **argv)
 {
-
-    // values.push_back({"Urgency", 1});
-    // values.push_back({"Time", 1});
-    // values.push_back({"Points", 1});
-    // Create GLUT window
     glutInit(&argc, argv);
 #ifdef __FREEGLUT_EXT_H__
     glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
@@ -370,42 +352,16 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
     glutInitWindowSize(1280, 720);
     glutCreateWindow("PrioritizationGUI");
-
-    // Setup GLUT display function
-    // We will also call ImGui_ImplGLUT_InstallFuncs() to get all the other functions installed for us,
-    // otherwise it is possible to install our own functions and call the imgui_impl_glut.h functions ourselves.
     glutDisplayFunc(glut_display_func);
 
-    // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
-    (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-
-    // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
 
-    // Setup Platform/Renderer backends
     ImGui_ImplGLUT_Init();
     ImGui_ImplGLUT_InstallFuncs();
     ImGui_ImplOpenGL2_Init();
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
 
     glutMainLoop();
 
@@ -416,5 +372,3 @@ int main(int argc, char **argv)
 
     return 0;
 }
-//0->2 float slider property weight;
-//0->25 int slider per value;
