add7bcb9ca81a9419ca8444e5db532fc01fd3560
William Katz <willkatz.dev@gmail.com>
2021-02-13 17:15:06 -0800

    :bug: MSVC Load func fix

--- include/file_io.hpp
+++ include/file_io.hpp
@@ -45,3 +45,33 @@ bool Load(const std::string &location, T &dest)
     dest = (T)temp;
     return true;
 }
+
+//THIS FUNCTION ONLY EXISTS BECAUSE MSVC WILL THROW A FIT ABOUT THE ABOVE LOAD FUNCTION (DO NOT REMOVE)
+template <typename T>
+bool Load(const std::string& location, std::vector<T>& dest)
+{
+    std::ifstream input(location);
+    json temp;
+    std::string temp_str;
+    if (input.is_open())
+    {
+        input >> temp_str;
+        if (temp_str == "")
+        {
+            return false;
+        }
+        temp = json::parse(temp_str);
+        input.close();
+    }
+    else
+    {
+        return false;
+    }
+
+    dest.resize(temp.size());
+    for (size_t i = 0; i < temp.size(); ++i)
+    {
+        dest[i] = (T)temp[i];
+    }
+    return true;
+}
